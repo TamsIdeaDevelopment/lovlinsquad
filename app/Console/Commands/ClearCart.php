16192056e3b2c08d210d191b0a08e9c6996b2d4a
TamsIdeaDevelopment <tamsidea.dev@gmail.com>
2021-02-27 13:40:43 +0000

    Testing Kernel for 2 Min

--- app/Console/Commands/ClearCart.php
+++ app/Console/Commands/ClearCart.php
@@ -42,8 +42,8 @@ class ClearCart extends Command
     public function handle()
     {
         $order = CartItem::where('status', 0)
-            ->where('created_at', '<=', now()->subHours(24)->toDateTimeString())
-//            ->where('created_at', '<=', now()->subMinutes(5)->toDateTimeString())
+//            ->where('created_at', '<=', now()->subHours(24)->toDateTimeString())
+            ->where('created_at', '<=', now()->subMinutes(2)->toDateTimeString())
             ->get();
 
         foreach($order as $key)
